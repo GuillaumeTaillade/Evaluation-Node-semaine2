3e8b218b258bcc98328752ebbd92c89858b7d85a
yuzurhia <phalyvong.tomy@gmail.com>
2023-11-24 15:33:51 +0100

    test

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-import express  from "express";
+import express from "express";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.js";
 
-
-
-dotenv.config
-();
+dotenv.config();
 const app = express();
 
-app.use("/",userRoute)
-
+app.use("/", userRoute);
 
 const PORT = 8000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
