2c86093bfb3a9ee4868d1ee350e030d345c93799
Mbo931 <momoha@live.fr>
2023-11-24 11:26:26 +0100

    Signup2

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -3,19 +3,23 @@ import {userSchema} from "modeles/userModele.js"
 
 
 
+
 export const signup = (req, res, next)=>{
+    const secret = process.env.Secret_Crypto
     const password = req.body.password
-    const sha256Hasher = crypto.createHmac("sha256", password);
+    const sha256Hasher = crypto.createHmac("sha256", secret );
+    const hash = sha256Hasher.update(password);
+
     try {
         const user= new userSchema({
             firstName :req.body.firstName,  
             lastName  :req.body.lastName,
             email :req.body.email, 
-            password  :sha256Hasher
+            password  :hash
         });
         user.save()
              .then(() => res.status(201).json({ message: 'Utilisateur crée !' }))
-                .catch(error => res.status(400).json({ error }));
+             .catch(error => res.status(400).json({ error }));
         }
     catch {
         (error => res.status(500).json({ error }))
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ dotenv.config();
 const app = express();
 
 
+
+
 const PORT = 8000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
